a0f962ac67a5762f279cb434279a75686c5b2a86
dennougorilla <dennougorilla@gmail.com>
2019-07-31 00:02:02 +0900

    make speech color

--- web/main.dart
+++ web/main.dart
@@ -4,15 +4,25 @@ void main() {
   final grammar =
       '#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;';
   final recognition = SpeechRecognition();
-  final speechRecognitionList = SpeechGrammarList()..addFromString(grammar, 1);
+  final speechRecognitionList = SpeechGrammarList()
+    ..addFromString(grammar, 1);
   recognition
+    ..continuous = true
     ..grammars = speechRecognitionList
     ..lang = 'en-US'
     ..interimResults = false
     ..maxAlternatives = 1;
 
-  final  dagnostic = document.querySelector('.output');
+  final diagnostic = document.querySelector('.output');
   final bg = document.querySelector('html');
-  document.body.onClick = recognition.start();
-  querySelector('#output').text = 'Your Dart app is running.';
+
+  document.body.onClick.listen((event) {
+    recognition.start();
+    print('Ready to receive a color command.');
+  });
+
+  recognition.onResult.listen((event) {
+    print(event);
+    print(event.resultIndex);
+  });
 }
